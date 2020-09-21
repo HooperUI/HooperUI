8e82abf6067b3053da79a08e4b4d5c07f5027ac0
Hooper <admin@hooperui.com>
2020-09-21 20:39:22 +0800

    add build

---
+++ build/dev.js
@@ -0,0 +1,9 @@
+/**
+ * @file: dev.js 开始开发
+ * 1. 启动 webpack，启动 dev-serve
+ * 2. 启动 mkdoc，在模板文件中引入 dev-serve 中的 HooperUI
+ * 3. 访问 mkdoc，在开发组件的时候，顺便编写文档即可
+ * @since: 2020-09-21
+ * @author: Hooper (admin@hooperui.com)
+ * @copyright: HooperUI @ Apache Licence 2.0
+ */
