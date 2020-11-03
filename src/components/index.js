ceb90794056759a59a7e832b3a230a7940a5068e
Hooper <admin@hooperui.com>
2020-11-03 20:13:02 +0800

    test ts declaration

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+/**
+ * @file: index.ts All components list
+ * !NOTICE: This file will be overwrite by build/build.js or build/dev.js
+ * @since: 2020-09-22
+ * @author: Hooper (admin@hooperui.com)
+ * @copyright: HooperUI @ Apache Licence 2.0
+ */
+// !DO NOT DELETE the comment below
+// codeHolder
+import Button from './button';
+const components = [Button];
+// holderEnd
+// !DO NOT DELETE the comment above
+// todo: install components
+export default {
+    Button,
+    components
+};
