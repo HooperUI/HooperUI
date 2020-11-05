70869940f331246b7bc8a813c44df29566b63f63
Hooper <admin@hooperui.com>
2020-11-05 17:53:50 +0800

    delete unnessury dirs

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,7 @@
 export interface Hello {
     name?: string
-};
+}
+
 export type AlertType = 'success' | 'warning' | 'info' | 'error'
 export type AlertEffect = 'dark' | 'light'
 
-const a = function(name?: Hello) {
-    return '1243';
-};
-a();
