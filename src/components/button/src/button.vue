579efc5e49c6887a15997c5f66a6e0d6905749a7
Hooper <admin@hooperui.com>
2020-12-18 19:36:05 +0800

    drop bem style

--- src/components/button/src/button.vue
+++ src/components/button/src/button.vue
@@ -2,13 +2,26 @@
 <script lang="ts">
 import {
     defineComponent,
-    Component,
     onMounted,
     reactive,
-    ref
+    ref,
+    PropType
 } from 'vue';
-const alert:Component = defineComponent({
+type buttonTypes = PropType<'primary' | 'success' | 'warning' | 'danger' | 'info' | 'link' | 'normal'>;
+type buttonSize = PropType<'large' | 'normal' | 'mini'>;
+
+export default defineComponent({
     name: 'HButton',
+    props: {
+        type: {
+            type: String as buttonTypes,
+            default: 'normal'
+        },
+        size: {
+            type: String as buttonSize,
+            default: 'normal'
+        }
+    },
     setup() {
         const count = ref(0);
         const content = reactive({title: 'Hello,HooperUI!'});
@@ -23,6 +36,5 @@ const alert:Component = defineComponent({
         };
     }
 });
-export default alert;
 </script>
 <style lang="sass" src="./button.scss"></style>
